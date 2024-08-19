ba93d9b36d6bcc1080c3fd91547f1d48a0890cd0
Carlos <carloseam94@gmail.com>
2024-08-19 23:13:55 +0200

    feat: metatags

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,3 +3,18 @@
     <slot></slot>
   </div>
 </template>
+<script setup lang="ts">
+const title = ref("Pokeguess");
+const description = ref(
+  "Have fun trying to guess the pokemon based on yes/no questions."
+);
+useHead({
+  title,
+  meta: [
+    {
+      name: "description",
+      content: description,
+    },
+  ],
+});
+</script>
